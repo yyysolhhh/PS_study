8667737971908b6471683ae6ea7610b71dff0f4f
Y <70841430+yyysolhhh@users.noreply.github.com>
2024-01-09 02:24:39 +0900

    [Silver IV] Title: 온라인 판매, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+P = []
+for _ in range(M):
+    P.append(int(input()))
+prices = sorted(P, reverse = True)
+profit = 0
+for i, p in enumerate(prices):
+    i = min(N - 1, i)
+    temp = p * (i + 1)
+    if profit < temp:
+        profit = max(profit, temp)
+        fixed_p = p
+print(fixed_p, profit)
