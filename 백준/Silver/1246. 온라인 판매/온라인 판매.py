b9131e9aa133340e3f3b8038913d260616b9c27d
Y <70841430+yyysolhhh@users.noreply.github.com>
2024-01-09 13:02:15 +0900

    [Silver IV] Title: 온라인 판매, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1246. 온라인 판매/온라인 판매.py
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 N, M = map(int, input().split())
 P = []
@@ -7,9 +6,9 @@ for _ in range(M):
     P.append(int(input()))
 prices = sorted(P, reverse = True)
 profit = 0
-for i, p in enumerate(prices):
-    i = min(N - 1, i)
-    temp = p * (i + 1)
+for i, p in enumerate(prices, start=1):
+    i = min(N, i)
+    temp = p * i
     if profit < temp:
         profit = temp
         fixed_p = p
