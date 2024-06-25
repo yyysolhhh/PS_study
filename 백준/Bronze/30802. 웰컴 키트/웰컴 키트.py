8e28b339d02de81c60bf7ed6951ce7569aaff043
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-06-25 22:11:33 +0900

    [Bronze III] Title: 웰컴 키트, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.py
@@ -3,11 +3,6 @@ input = sys.stdin.readline
 N = int(input())
 size = list(map(int, input().split()))
 T, P = map(int, input().split())
-tshirt = 0
-for i in size:
-    if i % T:
-        tshirt += i // T + 1
-    else:
-        tshirt += i // T
+tshirt = sum([(i // T + 1) if i % T else (i // T) for i in size])
 print(tshirt)
 print(N // P, N % P)
