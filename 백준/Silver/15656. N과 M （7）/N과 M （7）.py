9f08ed82bd6ab914276e5695b630356302b04902
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-03 20:43:15 +0900

    [Silver III] Title: N과 M (7), Time: 2016 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,13 @@
+def series():
+    if len(combi) == M:
+        print(' '.join(map(str, combi)))
+        return
+    for i in num:
+        combi.append(i)
+        series()
+        combi.pop()
+N, M = map(int, input().split())
+num = list(map(int, input().split()))
+num.sort()
+combi = []
+series()
