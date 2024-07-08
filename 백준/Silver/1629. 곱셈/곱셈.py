d72a770790f6caa6ab49bc352681d230c9efaf63
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-09 02:39:19 +0900

    [Silver I] Title: 곱셈, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+def DnC(a, b):
+    if b == 1:
+        return a % C
+    temp = DnC(a, b//2)
+    if b % 2 == 0:
+        return temp * temp % C
+    else:
+        return temp * temp * a % C
+A, B, C = map(int, input().split())
+print(DnC(A, B))
