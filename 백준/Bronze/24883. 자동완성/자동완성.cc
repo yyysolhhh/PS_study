eaa85a127787e64a3593b3695a33463e441c8e87
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-15 03:28:30 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  char al;
+  cin >> al;
+  if (al == 'N' or al == 'n')
+    cout << "Naver D2";
+  else
+    cout << "Naver Whale";
+  return 0;
+}
