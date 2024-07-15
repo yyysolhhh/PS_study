45360dde617ca120bcfd4d6b16cf3615208a2630
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-15 15:24:20 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -5,16 +5,16 @@ int dp[11];
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  dp[3] = 4;
+  for (int i = 4; i <= 11; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+  }
   cin >> T;
   while (T--) {
     int n;
     cin >> n;
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 4;
-    for (int i = 4; i <= n; i++) {
-      dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    }
     cout << dp[n] << '\n';
   }
   return 0;
