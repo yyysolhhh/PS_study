f4bec8e657f6949a81108a6fc170929d16a40676
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-16 23:52:59 +0900

    [Bronze III] Title: 오븐 시계, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,9 +1,6 @@
 A, B = map(int, input().split())
 C = int(input())
-B += C
-if B >= 60:
-    A += B // 60
-    B %= 60
-if A >= 24:
-    A %= 24
-print(A, B)
+total_min = A * 60 + B + C
+h = (total_min // 60) % 24
+m = total_min % 60
+print(h, m)
