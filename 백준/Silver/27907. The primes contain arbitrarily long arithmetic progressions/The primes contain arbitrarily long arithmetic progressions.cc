bd67ab4a73b090a93f7d40d120734fb947a72a03
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-16 22:37:09 +0900

    [Silver V] Title: The primes contain arbitrarily long arithmetic progressions, Time: 0 ms, Memory: 2020 KB, Score: 27907 point -BaekjoonHub

---
+++ 백준/Silver/27907. The primes contain arbitrarily long arithmetic progressions/The primes contain arbitrarily long arithmetic progressions.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  while (n--) {
+    cout << 2 << ' ';
+  }
+  return 0;
+}
