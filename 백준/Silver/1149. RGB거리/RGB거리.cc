a82dd979c2ef91bd1d17f8e5c61df8d27366faf6
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-17 22:35:24 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -20,8 +20,6 @@ int main(void) {
     D[i][1] = board[i][1] + min(D[i - 1][0], D[i - 1][2]);
     D[i][2] = board[i][2] + min(D[i - 1][0], D[i - 1][1]);
   }
-  int ans = min(D[N - 1][0], D[N - 1][1]);
-  ans = min(ans, D[N - 1][2]);
-  cout << ans;
+  cout << *min_element(D[N - 1], D[N - 1] + 3);
   return 0;
 }
