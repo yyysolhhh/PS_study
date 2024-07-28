b549f178919ed4a9b92a7e560edf53d525fd0dae
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-07-28 23:54:01 +0900

    [Bronze II] Title: 주사위 게임, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5566. 주사위 게임/주사위 게임.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int board[1001];
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N, M;
+  cin >> N >> M;
+  for (int i = 1; i <= N; i++) {
+    cin >> board[i];
+  }
+  int cur = 1;
+  for (int i = 1; i <= M; i++) {
+    int num;
+    cin >> num;
+    cur += num;
+    if (cur >= N) {
+      cout << i;
+      break;
+    }
+    cur += board[cur];
+    if (cur >= N) {
+      cout << i;
+      break;
+    }
+  }
+  return 0;
+}
