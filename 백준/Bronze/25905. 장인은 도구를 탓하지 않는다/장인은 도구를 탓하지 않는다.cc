7cd51179842009412ee990d08df6aabf79b39ead
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-08-24 23:35:30 +0900

    [Bronze II] Title: 장인은 도구를 탓하지 않는다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25905. 장인은 도구를 탓하지 않는다/장인은 도구를 탓하지 않는다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  double p[10];
+  for (int i = 0; i < 10; i++) {
+    cin >> p[i];
+  }
+  sort(p, p + 10);
+  double ans = 1;
+  for (int i = 1; i < 10; i++) {
+    ans *= p[i] / i;
+  }
+  cout.precision(6);
+  cout << fixed;
+  cout << ans * pow(10, 9);
+  return 0;
+}
