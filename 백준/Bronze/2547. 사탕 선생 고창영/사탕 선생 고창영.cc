4b389820b79c1ab9d0cf8820dc2a59b39df007eb
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-08-28 19:26:18 +0900

    [Bronze III] Title: 사탕 선생 고창영, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2547. 사탕 선생 고창영/사탕 선생 고창영.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(void) {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int T;
+  ll sum;
+  cin >> T;
+  while (T--) {
+    ll n;
+    cin >> n;
+    sum = 0;
+    for (int i = 0; i < n; i++) {
+      ll candy;
+      cin >> candy;
+      sum += candy % n;
+    }
+    if (sum % n == 0)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+  return 0;
+}
