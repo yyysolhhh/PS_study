ec68cd32d3ff47815863898a2841be639a53d015
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-09-04 23:59:45 +0900

    [Bronze III] Title: 소음, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2935. 소음/소음.cc
+++ 백준/Bronze/2935. 소음/소음.cc
@@ -9,23 +9,23 @@ int main(void) {
   string A, B;
   char op;
   cin >> A >> op >> B;
-  int a_digit = A.length();
-  int b_digit = B.length();
+  int a_zero = A.length() - 1;
+  int b_zero = B.length() - 1;
   if (op == '+') {
-    if (a_digit == b_digit) {
+    if (a_zero == b_zero) {
       cout << '2';
-      print_zero(a_digit - 1);
+      print_zero(a_zero);
     } else {
-      int big = max(a_digit, b_digit);
-      int small = min(a_digit, b_digit);
+      int big = max(a_zero, b_zero);
+      int small = min(a_zero, b_zero);
       cout << '1';
       print_zero(big - small - 1);
       cout << '1';
-      print_zero(small - 1);
+      print_zero(small);
     }
   } else if (op == '*') {
     cout << 1;
-    print_zero(a_digit + b_digit - 2);
+    print_zero(a_zero + b_zero);
   }
   return 0;
 }
