27cc446f0fa5c3db409706b14659d27b1c07f256
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-09-10 23:31:13 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+  int n;
+  cin >> n;
+  while (n--) {
+    int o;
+    cin >> o;
+    cout << o << ' ' << o << '\n';
+  }
+  return 0;
+}
