4414167a1a4e642bdb2f007b583e335ac4a8aefb
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-09-27 17:40:27 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M;
-vector<int> perm;
-void print() {
-  for (int i = 0; i < M; i++) {
-    cout << perm[i] << ' ';
-  }
-  cout << '\n';
-}
-void permutation() {
-  if (perm.size() == M) {
-    print();
+int n, m;
+int arr[10];
+bool isused[10];
+void func(int k) {
+  if (k == m) {
+    for (int i = 0; i < m; i++) cout << arr[i] << ' ';
+    cout << '\n';
     return;
   }
-  for (int i = 1; i <= N; i++) {
-    if (find(perm.begin(), perm.end(), i) != perm.end()) continue;
-    perm.push_back(i);
-    permutation();
-    perm.pop_back();
+  for (int i = 1; i <= n; i++) {
+    if (!isused[i]) {
+      arr[k] = i;
+      isused[i] = 1;
+      func(k + 1);
+      isused[i] = 0;
+    }
   }
 }
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> N >> M;
-  permutation();
-  return 0;
+  cin >> n >> m;
+  func(0);
 }
