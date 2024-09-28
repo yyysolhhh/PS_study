0896df6e7d4fa96495965b4ec9e4fedecde86b3e
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-09-28 11:56:34 +0900

    [Gold IV] Title: N-Queen, Time: 1580 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, cnt;
-int queen[15];
-bool visited[15];
-int is_dup(int cur, int i) {
-  for (int j = 0; j < cur; j++) {
-    if (queen[j] == i || j == cur || abs(queen[j] - i) == abs(cur - j))
-      return 1;
-  }
-  return 0;
-}
-void nqueen(int cur) {
-  if (cur == N) {
+bool isused1[40];
+bool isused2[40];
+bool isused3[40];
+int cnt = 0;
+int n;
+void func(int cur) {
+  if (cur == n) {
     cnt++;
     return;
   }
-  for (int i = 0; i < N; i++) {
-    if (visited[i] || is_dup(cur, i)) continue;
-    queen[cur] = i;
-    visited[i] = true;
-    nqueen(cur + 1);
-    visited[i] = false;
+  for (int i = 0; i < n; i++) {
+    if (isused1[i] || isused2[i + cur] || isused3[cur - i + n - 1]) continue;
+    isused1[i] = 1;
+    isused2[i + cur] = 1;
+    isused3[cur - i + n - 1] = 1;
+    func(cur + 1);
+    isused1[i] = 0;
+    isused2[i + cur] = 0;
+    isused3[cur - i + n - 1] = 0;
   }
 }
 int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> N;
-  nqueen(0);
+  cin >> n;
+  func(0);
   cout << cnt;
   return 0;
 }
