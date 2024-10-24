13597353ab0fc91a7fdcf42dea6249ced45fe6a6
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-10-24 23:07:59 +0900

    [Bronze II] Title: 지우개, Time: 32 ms, Memory: 31252 KB, Score: 35 point -BaekjoonHub

--- 백준/Bronze/21756. 지우개/지우개.py
+++ 백준/Bronze/21756. 지우개/지우개.py
@@ -1,2 +1,10 @@
 N = int(input())
-print(N // 2 + 1 if N % 2 else N)
+if N == 1:
+    print(1)
+elif N % 2:
+    if N // 2 % 2 == 0:
+        print(N - 1)
+    else:
+        print(N // 2 + 1)
+else:
+    print(N)
