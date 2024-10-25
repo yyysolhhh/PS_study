5c076edc9c347e1ad8ec76edd00d95cf781df719
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-10-26 00:17:02 +0900

    [Bronze I] Title: ISBN, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.py
+++ 백준/Bronze/14626. ISBN/ISBN.py
@@ -1,15 +1,13 @@
 ISBN = input()
-missing_index = ISBN.index('*')
+m_idx = ISBN.index("*")
 m = 0
-for i in range(12):
-    if i == missing_index:
+for i in range(len(ISBN)):
+    if i == m_idx:
         continue
-    digit = int(ISBN[i])
     weight = 1 if i % 2 == 0 else 3
-    m += digit * weight
-for x in range(10):
-    weight = 1 if missing_index % 2 == 0 else 3
-    check_sum = m + x * weight
-    if (check_sum + int(ISBN[-1])) % 10 == 0:
-        print(x)
+    m += int(ISBN[i]) * weight
+for i in range(10):
+    weight = 1 if m_idx % 2 == 0 else 3
+    if (m + i * weight) % 10 == 0:
+        print(i)
         break
