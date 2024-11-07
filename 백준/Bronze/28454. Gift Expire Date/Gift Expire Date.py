0daf850f1da78a2663c056f68ab077a2428832c8
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-11-07 23:40:55 +0900

    [Bronze III] Title: Gift Expire Date, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.py
+++ 백준/Bronze/28454. Gift Expire Date/Gift Expire Date.py
@@ -1,14 +1,6 @@
-now = list(map(int, input().split('-')))
+now = input()
 N = int(input())
 ans = 0
 for _ in range(N):
-    exp = list(map(int, input().split('-')))
-    if now[0] < exp[0]:
-        ans += 1
-    elif now[0] == exp[0]:
-        if now[1] < exp[1]:
-            ans += 1
-        elif now[1] == exp[1]:
-            if now[2] <= exp[2]:
-                ans += 1
+    ans += now <= input()
 print(ans)
