8ec786edc82de91df6a4e7ef421f1eae032c8110
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-11-26 21:11:24 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,5 @@
+room = {"Algorithm": 204, "DataAnalysis": 207, "ArtificialIntelligence": 302, "CyberSecurity": "B101", "Network": 303, "Startup": 501, "TestStrategy": 105}
+N = int(input())
+for _ in range(N):
+    seminar = input()
+    print(room[seminar])
