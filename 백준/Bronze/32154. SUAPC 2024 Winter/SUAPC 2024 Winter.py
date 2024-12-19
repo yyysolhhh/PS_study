4724aa8a533b045384940cd41d7c266608410f32
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-12-19 23:41:13 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,4 @@
+N = int(input())
+result = [(), (11, "A B C D E F G H J L M"), (9, "A C E F G H I L M"), (9, "A C E F G H I L M"), (9, "A B C E F G H L M"), (8, "A C E F G H L M"), (8, "A C E F G H L M"), (8, "A C E F G H L M"), (8, "A C E F G H L M"), (8, "A C E F G H L M"), (8, "A B C F G H L M")]
+print(result[N][0])
+print(result[N][1])
