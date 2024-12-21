6794b10b73c8eb04554dd939af46cfe47bb0208c
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2024-12-21 23:46:57 +0900

    [Bronze V] Title: Football Scoring, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/24736. Football Scoring/Football Scoring.py
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -1,6 +1,4 @@
-visiting = list(map(int, input().split()))
-home = list(map(int, input().split()))
 score = [6, 3, 2, 1, 2]
-v_score = sum(i * j for i, j in zip(visiting, score))
-h_score = sum(i * j for i, j in zip(home, score))
-print(v_score, h_score)
+for _ in range(2):
+    team = list(map(int, input().split()))
+    print(sum(i * j for i, j in zip(team, score)), end=" ")
