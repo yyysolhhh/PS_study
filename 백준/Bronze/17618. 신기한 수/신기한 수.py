f8dbb5f05bdbb1ebc61e9bf205e8fd0ed31d13ec
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-01 23:50:04 +0900

    [Bronze III] Title: 신기한 수, Time: 2724 ms, Memory: 112320 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,9 +1,7 @@
 N = int(input())
 ans = 0
 for num in range(1, N + 1):
-    total = 0
-    for i in str(num):
-        total += int(i)
+    total = sum(int(i) for i in str(num))
     if total == 0:
         continue
     ans += (num % total == 0)
