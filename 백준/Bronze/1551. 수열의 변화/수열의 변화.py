0f2a09dc262770493c261a4bdced6a558cec045b
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-05 18:04:44 +0900

    [Bronze I] Title: 수열의 변화, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1551. 수열의 변화/수열의 변화.py
@@ -0,0 +1,8 @@
+N, K = map(int, input().split())
+seq = list(map(int, input().split(",")))
+for _ in range(K):
+    temp = []
+    for i in range(len(seq) - 1):
+        temp.append(seq[i + 1] - seq[i])
+    seq = temp
+print(",".join(map(str, seq)))
