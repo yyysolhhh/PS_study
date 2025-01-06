8e085f2f0491742d803da228a66b0eaaa31d7d12
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-06 23:35:00 +0900

    [Silver V] Title: 자료구조는 정말 최고야, Time: 252 ms, Memory: 139236 KB -BaekjoonHub

---
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+ans = "Yes"
+for _ in range(M):
+    k = int(input())
+    dummy = list(map(int, input().split()))
+    for i in range(k - 1):
+        if dummy[i + 1] - dummy[i] > 0:
+            ans = "No"
+            break
+print(ans)
