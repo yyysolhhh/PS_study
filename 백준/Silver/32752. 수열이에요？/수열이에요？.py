2ee950fe560d01fb1ba165d08e76c2d49a824944
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-07 23:51:09 +0900

    [Silver V] Title: 수열이에요?, Time: 88 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Silver/32752. 수열이에요？/수열이에요？.py
+++ 백준/Silver/32752. 수열이에요？/수열이에요？.py
@@ -1,9 +1,4 @@
 N, L, R = map(int, input().split())
 A = list(map(int, input().split()))
-ans = 1
-A = A[:L - 1] + sorted(A[L - 1:R]) + A[R:]
-for i in range(N - 1):
-    if A[i] > A[i + 1]:
-        ans = 0
-        break
-print(ans)
+A[L - 1:R] = sorted(A[L - 1:R])
+print(int(all(A[i] <= A[i + 1] for i in range(N - 1))))
