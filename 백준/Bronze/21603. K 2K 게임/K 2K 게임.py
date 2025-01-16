723ec0b5b2237516256542236ca0bda23bf31aea
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-16 23:56:02 +0900

    [Bronze III] Title: K 2K 게임, Time: 88 ms, Memory: 37424 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -1,8 +1,9 @@
 N, K = map(int, input().split())
 nums = []
 fK = K % 10
+fK2 = 2 * K % 10
 for i in range(1, N + 1):
-    if i % 10 != fK and i % 10 != 2 * fK:
+    if i % 10 != fK and i % 10 != fK2:
         nums.append(i)
 print(len(nums))
 print(*nums)
