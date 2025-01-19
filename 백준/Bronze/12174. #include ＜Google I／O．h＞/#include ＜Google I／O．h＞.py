575d86237a153a463fd12db431327a6df78046e4
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-01-20 00:00:54 +0900

    [Bronze II] Title: #include <Google I/O.h>, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/12174. #include ＜Google I／O．h＞/#include ＜Google I／O．h＞.py
+++ 백준/Bronze/12174. #include ＜Google I／O．h＞/#include ＜Google I／O．h＞.py
@@ -2,6 +2,5 @@ T = int(input())
 for i in range(1, T + 1):
     B = int(input())
     case = input().replace("O", "0").replace("I", "1")
-    print(f"Case #{i}: ", end="")
     ans = [chr(int(case[i:i + 8], 2)) for i in range(0, len(case), 8)]
-    print("".join(ans))
+    print(f"Case #{i}: {"".join(ans)}")
