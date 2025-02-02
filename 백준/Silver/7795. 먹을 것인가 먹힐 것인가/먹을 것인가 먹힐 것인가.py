9b3932b8b727b79b5a772a61e29239541b5cc8d1
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-02-02 23:55:30 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 280 ms, Memory: 35668 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -16,7 +16,7 @@ def binarysearch(n):
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    A = sorted(map(int, input().split()))
+    A = list(map(int, input().split()))
     B = sorted(map(int, input().split()))
     ans = 0
     for i in A:
