97f1c7c57b2a0d071d62817d1bb99d49d4fbd7b8
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-03-09 00:13:00 +0900

    [Bronze I] Title: 히든 넘버, Time: 1520 ms, Memory: 77160 KB -BaekjoonHub

--- 백준/Bronze/8595. 히든 넘버/히든 넘버.py
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.py
@@ -1,15 +1,10 @@
 n = int(input())
-arr = input()
-num = ''
-val = 0
-
-for i in arr:
-  if '0' <= i and i <= '9':
-    num += i
-  elif num != '':
-    val += int(num)
-    num = ''
-if num != '':
-  val += int(num)
-  
-print(val)
+word = input()
+hid = []
+for i in range(len(word)):
+    if word[i].isdigit():
+        if i > 0 and word[i - 1].isdigit():
+            hid[-1] += word[i]
+        else:
+            hid.append(word[i])
+print(sum(map(int, hid)))
