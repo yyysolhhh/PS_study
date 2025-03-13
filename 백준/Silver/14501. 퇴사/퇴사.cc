7c5572bf229217d2a692fb3f79237ad2bba4ebd9
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-03-13 18:02:31 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,19 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int N;
-int dp[17], T[17], P[17];
-int main() {
+int t[20], p[20], d[20];
+int main(void) {
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> N;
-  for (int i = 1; i <= N; i++) {
-    cin >> T[i] >> P[i];
+  int n;
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+      cin >> t[i] >> p[i];
   }
-  for (int i = 1; i <= N; i++) {
-    dp[i] = max(dp[i], dp[i - 1]);
-    if (i + T[i] - 1 <= N) dp[i + T[i]] = max(dp[i + T[i]], dp[i] + P[i]);
+  for (int i = n; i >= 1; i--) {
+    if (i + t[i] <= n + 1) {
+      d[i] = max(d[i + t[i]] + p[i], d[i + 1]);
+    } else
+      d[i] = d[i + 1];
   }
-  cout << max(dp[N], dp[N + 1]);
-  return 0;
+  cout << *max_element(d, d + n + 1);
 }
