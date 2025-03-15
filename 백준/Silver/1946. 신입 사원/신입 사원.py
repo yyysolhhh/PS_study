ab550ad46e3d29b9f840f76115bf632cf7e23d00
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-03-16 00:06:22 +0900

    [Silver I] Title: 신입 사원, Time: 4928 ms, Memory: 215576 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,12 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    appl = [list(map(int, input().split())) for _ in range(N)]
+    appl.sort()
+    cnt = 1
+    rank = appl[0][1]
+    for i in range(1, N):
+        if rank > appl[i][1]:
+            cnt += 1
+            rank = appl[i][1]
+    print(cnt)
