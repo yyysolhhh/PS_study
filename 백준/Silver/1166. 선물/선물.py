6f46492dcdc88ce5ca1ddda8d1f84353f477b755
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-03-23 15:14:38 +0900

    [Silver III] Title: 선물, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1166. 선물/선물.py
+++ 백준/Silver/1166. 선물/선물.py
@@ -1,14 +1,17 @@
-def binary_search(start, end):
-    for _ in range(100):
-        mid = (start + end) / 2
-        cnt = (L // mid) * (W // mid) * (H // mid)
-        if cnt < N:
-            end = mid
-        else:
-            start = mid
-    return float(mid)
+import sys
+input = sys.stdin.readline
 
+def binary_search(N, L, W, H):
+    left, right = 0, min(L, W, H)
+    for _ in range(100):
+        mid = (left + right) / 2
+        num = (L // mid) * (W // mid) * (H // mid)
+        if num >= N:
+            left = mid
+        else:
+            right = mid
+    return left
 
 N, L, W, H = map(int, input().split())
-start, end = 0, min(L, W, H)
-print(binary_search(start, end))
+ans = binary_search(N, L, W, H)
+print(f"{ans:.10f}")
