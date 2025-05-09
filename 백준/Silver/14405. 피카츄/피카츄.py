1360424a638e00df47200d252204dd6e2194d79e
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-05-09 23:59:59 +0900

    [Silver V] Title: 피카츄, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.py
+++ 백준/Silver/14405. 피카츄/피카츄.py
@@ -1,14 +1,10 @@
-p = ["pi", "ka", "chu"]
-S = input()
-i = 0
-while i < len(S):
-    if S[i:i + 2] in p:
-        i += 2
-    elif S[i: i + 3] in p:
-        i += 3
-    else:
-        break
-if i < len(S):
-    print("NO")
-else:
+s = input()
+
+s = s.replace("pi", " ")
+s = s.replace("ka", " ")
+s = s.replace("chu", " ")
+
+if len(s.strip()) == 0:
     print("YES")
+else:
+    print("NO")
