da50f10e6353546e1fde9018c27774553cc8ddba
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-07-12 23:59:41 +0900

    [Silver V] Title: 칠무해, Time: 4528 ms, Memory: 542248 KB -BaekjoonHub

--- 백준/Silver/14729. 칠무해/칠무해.py
+++ 백준/Silver/14729. 칠무해/칠무해.py
@@ -1,8 +1,6 @@
 import sys
-
-N = int(sys.stdin.readline())
-
-score_list = sorted([float(sys.stdin.readline()) for _ in range(N)])
-
-for i in range(7):
-    print(f'{score_list[i]:.3f}')
+input = sys.stdin.readline
+N = int(input())
+scores = [float(input()) for _ in range(N)]
+for i in sorted(scores)[:7]:
+    print(f"{i:.3f}")
