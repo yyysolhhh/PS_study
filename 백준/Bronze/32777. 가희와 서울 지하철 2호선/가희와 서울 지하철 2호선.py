6bddc2e0d7d4ebe0a514b0dada88b32efc32e1b5
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-07-14 22:38:56 +0900

    [Bronze III] Title: 가희와 서울 지하철 2호선, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32777. 가희와 서울 지하철 2호선/가희와 서울 지하철 2호선.py
+++ 백준/Bronze/32777. 가희와 서울 지하철 2호선/가희와 서울 지하철 2호선.py
@@ -1,15 +1,12 @@
 Q = int(input())
-
 for _ in range(Q):
     a, b = map(int, input().split())
-
-    if a < b:
-        inner = b - a
-        outer = 43 - (b - a)
-    else:
+    if a > b:
         inner = 43 - (a - b)
         outer = a - b
-
+    else:
+        inner = b - a
+        outer = 43 - (b - a)
     if inner < outer:
         print("Inner circle line")
     elif inner > outer:
