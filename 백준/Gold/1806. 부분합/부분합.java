8323485444b1c2d53ce5dc42f90987aed78c8b46
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-10-11 11:09:41 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 23916 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,43 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int N, M;
+    static int[] nums;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String str = bf.readLine();
-        StringTokenizer st = new StringTokenizer(str);
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-        String str2 = bf.readLine();
-        StringTokenizer st2 = new StringTokenizer(str2);
-        int[] series = new int[N+1];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄을 읽어서 " " \t \n \r \f 기준으로 나눠줌
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N + 1]; // 하나 크게 만들어주기 -> 투 포인터 편하게 쓸 수 있음
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            series[i] = Integer.parseInt(st2.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
-        int l = 0;
-        int h = 0;
-        int sum = series[0];
-        int length = N + 1;
-        while (l < N && h < N) {
-            int temp = N + 1;
-            // sum >= S -> l++, temp
-            if (sum >= S) {
-                temp = h - l + 1;
-                sum -= series[l++];
-            } else { // sum < S -> h++
-                sum += series[++h];
+        int low = 0, high = 0, sum = nums[0], minLength = Integer.MAX_VALUE;
+        while (true) {
+            // sum >= M -> 답, low++
+            if (sum >= M) {
+                minLength = Math.min(high - low + 1, minLength);
+                sum -= nums[low++];
             }
-            // temp < length -> length = temp
-            length = Math.min(temp, length);
+            // sum > M -> low++
+            else {
+                sum += nums[++high];
+            }
+            if (high == N) {
+                break;
+            }
         }
-
-        if (length == N + 1){
+        if (minLength == Integer.MAX_VALUE) {
             System.out.println(0);
         } else {
-            System.out.println(length);
+            System.out.println(minLength);
         }
     }
 }
