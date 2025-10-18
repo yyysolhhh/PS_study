e6a1d5e46b9019e515effead82abff8818113475
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-10-18 21:22:16 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+int lc[27], rc[27];
+
+void preorder(int cur) {
+  cout << char(cur + 'A' - 1);
+  if (lc[cur] != 0) preorder(lc[cur]);
+  if (rc[cur] != 0) preorder(rc[cur]);
+}
+
+void inorder(int cur) {
+  if (lc[cur] != 0) inorder(lc[cur]);
+  cout << char(cur + 'A' - 1);
+  if (rc[cur] != 0) inorder(rc[cur]);
+}
+
+void postorder(int cur) {
+  if (lc[cur] != 0) postorder(lc[cur]);
+  if (rc[cur] != 0) postorder(rc[cur]);
+  cout << char(cur + 'A' - 1);
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    char c, l, r;
+    cin >> c >> l >> r;
+    if (l != '.') lc[c - 'A' + 1] = l - 'A' + 1;
+    if (r != '.') rc[c - 'A' + 1] = r - 'A' + 1;
+  }
+  preorder(1);
+  cout << '\n';
+  inorder(1);
+  cout << '\n';
+  postorder(1);
+  cout << '\n';
+  return 0;
+}
