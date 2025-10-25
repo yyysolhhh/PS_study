75b49c94b26cdd845ed0222a81b4e57a6130cc05
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-10-25 18:44:40 +0900

    [Gold III] Title: 줄 세우기, Time: 24 ms, Memory: 3956 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.cc
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.cc
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M;
 vector<int> adj[32001];
 int deg[32001];
-queue<int> q;
-vector<int> result;
+int N, M;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
   cin >> N >> M;
   for (int i = 0; i < M; i++) {
     int A, B;
@@ -16,17 +13,17 @@ int main() {
     adj[A].push_back(B);
     deg[B]++;
   }
+  queue<int> q;
   for (int i = 1; i <= N; i++) {
     if (deg[i] == 0) q.push(i);
   }
   while (!q.empty()) {
     int cur = q.front(); q.pop();
-    result.push_back(cur);
+    cout << cur << ' ';
     for (int nxt: adj[cur]) {
       deg[nxt]--;
       if (deg[nxt] == 0) q.push(nxt);
     }
   }
-  for (int x: result) cout << x << ' ';
   return 0;
 }
