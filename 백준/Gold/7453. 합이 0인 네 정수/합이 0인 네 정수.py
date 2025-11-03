92ecf7b794bdbc6efc23719dc2acf763c8cae11b
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-11-03 18:14:51 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 7132 ms, Memory: 671436 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.py
@@ -10,8 +10,25 @@ for i in range(n):
     C.append(c)
     D.append(d)
 ans = 0
-AB = [a + b for a in A for b in B]
-CD = Counter([c + d for c in C for d in D])
-for i in AB:
-    ans += CD[-i]
+AB = sorted(a + b for a in A for b in B)
+CD = sorted(c + d for c in C for d in D)
+ab, cd = 0, len(CD) - 1
+while ab < len(AB) and cd >= 0:
+    tot = AB[ab] + CD[cd]
+    if tot == 0:
+        cur_ab = ab
+        cur_cd = cd
+        ab_cnt = 0
+        while ab < len(AB) and AB[ab] == AB[cur_ab]:
+            ab += 1
+            ab_cnt += 1
+        cd_cnt = 0
+        while cd >= 0 and CD[cd] == CD[cur_cd]:
+            cd -= 1
+            cd_cnt += 1
+        ans += ab_cnt * cd_cnt
+    elif tot < 0:
+        ab += 1
+    else:
+        cd -= 1
 print(ans)
