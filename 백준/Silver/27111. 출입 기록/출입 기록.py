f860d899ef45110ba90f3408b8bf4d3aa6158a3c
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2025-12-20 23:30:47 +0900

    [Silver V] Title: 출입 기록, Time: 224 ms, Memory: 41632 KB -BaekjoonHub

--- 백준/Silver/27111. 출입 기록/출입 기록.py
+++ 백준/Silver/27111. 출입 기록/출입 기록.py
@@ -1,22 +1,19 @@
 import sys
 input = sys.stdin.readline
 N = int(input())
-inout = {}
+inout = set()
 ans = 0
 for _ in range(N):
     a, b = map(int, input().split())
-    if a in inout:
-        if inout[a] == 1:
-            if b == 1:
-                ans += 1
-        else:
-            if b == 0:
-                ans += 1
-    else:
-        if b == 0:
+    if b == 1:
+        if a in inout:
             ans += 1
-    inout[a] = b
-for _, v in inout.items():
-    if v == 1:
-        ans += 1
+        else:
+            inout.add(a)
+    else:
+        if a not in inout:
+            ans += 1
+        else:
+            inout.remove(a)
+ans += len(inout)
 print(ans)
