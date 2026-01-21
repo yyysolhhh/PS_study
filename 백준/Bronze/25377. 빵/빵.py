6e1f4cc21eea68b257ac0a6b240f1dd10529d33b
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2026-01-21 23:55:29 +0900

    [Bronze IV] Title: 빵, Time: 36 ms, Memory: 32412 KB, Score: 70 point -BaekjoonHub

--- 백준/Bronze/25377. 빵/빵.py
+++ 백준/Bronze/25377. 빵/빵.py
@@ -1,3 +1,5 @@
+import sys
+input = sys.stdin.readline
 N = int(input())
 time = 10000
 ans = -1
@@ -5,4 +7,5 @@ for _ in range(N):
     A, B = map(int, input().split())
     if B - A >= 0 and time > B - A :
         ans = B
+        time = B - A
 print(ans)
