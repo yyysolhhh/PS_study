155612e23ba4122cfecaa84c16c8f4e18b7ab6fa
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2026-02-17 00:02:51 +0900

    [Bronze IV] Title: Zero or One, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13623. Zero or One/Zero or One.py
+++ 백준/Bronze/13623. Zero or One/Zero or One.py
@@ -1,9 +1,9 @@
 A, B, C = map(int, input().split())
-if A != B and A != C:
-    print("A")
-elif B != A and B != C:
-    print("B")
-elif C != A and C != A:
-    print("C")
-else:
+if A == B == C:
     print("*")
+elif A == B:
+    print("C")
+elif A == C:
+    print("B")
+else:
+    print("A")
