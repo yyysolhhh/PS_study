e56f987f7559d9760122c20c58fe4ee4ac40fe7c
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2026-03-23 01:44:58 +0900

    [Bronze I] Title: 포도주 상인, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/34850. 포도주 상인/포도주 상인.py
+++ 백준/Bronze/34850. 포도주 상인/포도주 상인.py
@@ -1,9 +1,8 @@
+import sys
+input = sys.stdin.readline
 x, y, p, a, b = map(int, input().split())
 ans = 0
-if b > 0:
-    max_p = p + (y - 1) * b
-else:
-    max_p = p
+max_p = p + (y - 1) * b
 while x > 0:
     ans += max_p
     max_p -= a
