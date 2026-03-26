56c9af13e54ff1bc55eb2b3f5176569c8a021b6f
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2026-03-26 23:48:22 +0900

    [Bronze II] Title: 배수들의 합, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17173. 배수들의 합/배수들의 합.py
+++ 백준/Bronze/17173. 배수들의 합/배수들의 합.py
@@ -1,8 +1,9 @@
+import sys
+input = sys.stdin.readline
 N, M = map(int, input().split())
 K = list(map(int, input().split()))
 nums = set()
 for i in K:
-    for j in range(N + 1):
-        if j % i == 0:
-            nums.add(j)
+    for j in range(i, N + 1, i):
+        nums.add(j)
 print(sum(nums))
