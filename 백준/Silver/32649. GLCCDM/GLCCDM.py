a6335cedf6e4b1ce5ccc1f9e697082c82eda9a9c
yyysolhhh <70841430+yyysolhhh@users.noreply.github.com>
2026-04-01 01:22:29 +0900

    [Silver III] Title: GLCCDM, Time: 144 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/32649. GLCCDM/GLCCDM.py
+++ 백준/Silver/32649. GLCCDM/GLCCDM.py
@@ -1,11 +1,14 @@
 A, B, K = map(int, input().split())
 candi = []
 ans = [A, B]
-for i in range(A, B + 1, A):
-    if B % i == 0:
-        candi.append(i)
-if len(candi) < K:
+if B % A != 0:
     print(-1)
 else:
-    ans.extend(candi[1:K - 1])
-    print(*ans)
+    for i in range(A, B + 1, A):
+        if B % i == 0:
+            candi.append(i)
+    if len(candi) < K:
+        print(-1)
+    else:
+        ans.extend(candi[1:K - 1])
+        print(*ans)
